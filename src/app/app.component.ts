c3f1bbbf1f6a7d3e90416413dbbd1851532ebbad
Magali Medina <magalimedina96@gmail.com>
2026-04-01 09:24:17 -0300

    fix theme and PDF download

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,8 +35,16 @@ export class AppComponent {
   }
 
   toggleTheme() {
-    document.body.classList.toggle('dark-mode');
-    document.body.classList.toggle('light-mode');
+    // document.body.classList.toggle('dark-mode');
+    // document.body.classList.toggle('light-mode');
+    const body = document.body;
+      if (body.classList.contains('dark-theme')) {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+      } else {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+      }
   }
 
 }
