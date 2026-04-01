f32fb2f6ecbc18fea68dff57cb7af1df1557382e
Magali Medina <magalimedina96@gmail.com>
2026-04-01 09:08:55 -0300

    se agrego angular material y datos al app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbar } from "@angular/material/toolbar";
+import { MatIcon } from "@angular/material/icon";
+import { MatSidenavContainer, MatSidenav, MatSidenavContent } from "@angular/material/sidenav";
+import { MatCard, MatCardTitle, MatCardContent } from "@angular/material/card";
+import { MatList, MatListItem } from "@angular/material/list";
+import { TranslateService } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatSidenavContainer, MatSidenav, MatCard, MatCardTitle, MatCardContent, MatList, MatListItem, MatSidenavContent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mi-cv';
+  //constructor(private translate: TranslateService) {
+    //this.translate.setDefaultLang('es');
+  //}
+
+  switchLang(lang: string) {
+    //this.translate.use(lang);
+  }
+
+  downloadCV() {
+    window.open('assets/CV_Magali_Medina.pdf', '_blank');
+  }
+
+  goToGithub() {
+    window.open('https://github.com/magaliamedina', '_blank');
+  }
+
+  toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+  }
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,43 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { importProvidersFrom } from '@angular/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
+export function HttpLoaderFactory() {
+  return new TranslateHttpLoader(); // ✅ solo http
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+    importProvidersFrom(
+      MatToolbarModule,
+      MatButtonModule,
+      MatIconModule,
+      MatSidenavModule,
+      MatListModule,
+      MatCardModule,
+      TranslateModule.forRoot({
+        defaultLanguage: 'es',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+
+    )
+  ]
+});
